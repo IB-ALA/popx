b590cb1abd5aee21f876d9845dd6916f2eddcf96
IBALA <ishaqibrahimyusif@gmail.com>
2025-06-05 00:38:06 +0000

    minor css tweaks

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -5,22 +5,27 @@ const Home = () => {
     <div className="home-container">
       <h3 className="title">Account Settings</h3>
 
-      <div className="profile-card">
-        <img
-          className="profile-image"
-          src="https://via.placeholder.com/80"
-          alt="User"
-        />
-        <div className="profile-info">
-          <h4 className="profile-name">Marry Doe</h4>
-          <p className="profile-email">Marry@Gmail.Com</p>
+      <div className="account-section">
+        <div className="profile-card">
+          <img
+            className="profile-image"
+            src="https://via.placeholder.com/80"
+            alt="User"
+          />
+          <div className="profile-info">
+            <h4 className="profile-name">Marry Doe</h4>
+            <p className="profile-email">Marry@Gmail.Com</p>
+          </div>
         </div>
+
+        <p className="description">
+          Lorem Ipsum Dolor Sit Amet. Consetetur Sadipscing Elitr. Sed Diam
+          Nonumy Eirmod Tempor Invidunt Ut Labore Et Dolore Magna Aliquyam Erat.
+          Sed Diam
+        </p>
       </div>
 
-      <p className="description">
-        Lorem Ipsum Dolor Sit Amet. Consetetur Sadipscing Elitr. Sed Diam Nonumy
-        Eirmod Tempor Invidunt Ut Labore Et Dolore Magna Aliquyam Erat. Sed Diam
-      </p>
+      <div className="divider"></div>
     </div>
   );
 };
